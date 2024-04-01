c81d94cbbd72af0693bdb6317c26c17c582417a5
omdholariya07 <omdholariya501gmail.com>
2024-04-01 19:14:23 +0530

    create table to manage cpt and add cpt in table or database

--- wp-content/plugins/alecaddd-plugin/inc/Base/Activate.php
+++ wp-content/plugins/alecaddd-plugin/inc/Base/Activate.php
@@ -1,20 +1,22 @@
 <?php
 /**
- * @package Alecadddplugin
+ * @package  AlecadddPlugin
  */
-
 namespace Inc\Base;
 
-class Activate {
-    public static function activate() {
-        flush_rewrite_rules();
-        
-       if(get_option('alecaddd_plugin')){
-           return;
-       }
-        
-        $default = array();
-        
-       update_option('alecaddd_plugin',$default);
-    }
+class Activate
+{
+	public static function activate() {
+		flush_rewrite_rules();
+
+		$default = array();
+
+		if ( ! get_option( 'alecaddd_plugin' ) ) {
+			update_option( 'alecaddd_plugin', $default );
+		}
+
+		if ( ! get_option( 'alecaddd_plugin_cpt' ) ) {
+			update_option( 'alecaddd_plugin_cpt', $default );
+		}
+	}
 }
